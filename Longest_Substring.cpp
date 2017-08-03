5ab536357c1e58f18b3b755f7fa633658b634adc
olaoluwa Adesanya <olaolu961@gmail.com>
2017-08-02 17:25:19 -0700

    Replaced  Longest_Substring function

--- Longest_Substring.cpp
+++ Longest_Substring.cpp
@@ -4,36 +4,46 @@
 #include<set>
 using namespace std;
 
-int lengthOfLongestSubstring(string s)
+std::set<string> build_sub_string(string s)
 {
-    int max=0;
-    int count=0;
-    std::set<char> seen;
-    for(char c:s)
+    std::set<string> subs;
+    int size=s.size();
+    for(int wsize=1;wsize<=size;++wsize)
     {
-        if(isalpha(c))
-	{
-	    c=tolower(c);
-	    if(seen.find(c)==seen.end())//not in seen
-            {
-                ++count;
-               if(count>max)
-               {
-                   max=count;
-  
-               }
-            }
-            else
-            {
-                count=1;
-            }
-            seen.insert(c);
+        for(int j=0;j<=size;++j)
+        { 
+            subs.insert(s.substr(j,wsize));
         }
     }
-    return max;    
+    
+    return subs;
+}
+
+int unique_len(string s)
+{
+    std::set<char> s_set;
+    for(char c:s)
+        s_set.insert(c);
+    int s_size=s.size();
+    int set_size=s_set.size();
+    if(s_size==set_size)
+    	return set_size;
+    return 0;
 }
 
 
+int lengthOfLongestSubstring(string s) 
+{
+    int max=0;
+    std::set<string> allsubs=build_sub_string(s);
+    for(std::set<string>::iterator each=allsubs.begin();each!=allsubs.end();++each)
+    {
+        int current=unique_len(*each);
+        if(current>max)
+           max=current;
+    }
+    return max;
+}
 
 int main()
 {
@@ -41,29 +51,19 @@ int main()
     string s1="aabcabcbb";
     string s2="bbbbb";
     string s3="pwwkew";
-    string s4=" ";
-    string s5="12221112345";
-    string s6="!@#&&**@";
-    string s7="!@#&&aabcabcbb";
+    string s4="abacde";
 
     assert(lengthOfLongestSubstring(s1)==3);
     assert(lengthOfLongestSubstring(s2)==1);
     assert(lengthOfLongestSubstring(s3)==3);
-    assert(lengthOfLongestSubstring(s4)==0);
-    assert(lengthOfLongestSubstring(s5)==0);
-    assert(lengthOfLongestSubstring(s6)==0);
-    assert(lengthOfLongestSubstring(s7)==3);
+    assert(lengthOfLongestSubstring(s4)==5);
 
     //Sample output
     cout<<"Word: "<<s1<<" Max:"<<lengthOfLongestSubstring(s1)<<endl;
     cout<<"Word: "<<s2<<" Max:"<<lengthOfLongestSubstring(s2)<<endl;
     cout<<"Word: "<<s3<<" Max:"<<lengthOfLongestSubstring(s3)<<endl;
     cout<<"Word: "<<s4<<" Max:"<<lengthOfLongestSubstring(s4)<<endl;  
-    cout<<"Word: "<<s5<<" Max:"<<lengthOfLongestSubstring(s5)<<endl;
-    cout<<"Word: "<<s6<<" Max:"<<lengthOfLongestSubstring(s6)<<endl;
-    cout<<"Word: "<<s7<<" Max:"<<lengthOfLongestSubstring(s7)<<endl;
-   
-
+  
     return 0;  
 
 };
